996dc279ba3e2f238ed1bc2860fba5e2a2595cf6
PauBlanes_ENTI <paublanespardo@enti.cat>
2018-03-07 20:18:47 +0100

    Projecte pujat

---
+++ Pong+Arcanoid/Assets/Scripts/ballScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballScript : MonoBehaviour {
+
+	Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		rb.velocity = new Vector2(0,5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+		/*Vector2 normal = coll.transform.parent.position - coll.transform.parent.position;
+		Debug.DrawLine (coll.transform.parent.position, coll.transform.parent.position, Color.red);
+		rb.velocity *= -1;*/
+	}
+}
---
+++ Pong+Arcanoid/Assets/Scripts/rotate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour {
+    public float speed = 120;
+    public GameObject center;
+    public bool down = false;
+    float min, max;
+	// Use this for initialization
+	void Start () {
+        if (down)
+        {
+            
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (down)
+        {
+            if (Input.GetKey(KeyCode.LeftArrow) && (center.transform.rotation.eulerAngles.z - Time.deltaTime * speed) > 195)
+            {
+                center.transform.Rotate(new Vector3(0,0,-Time.deltaTime* speed));
+            }
+            if (Input.GetKey(KeyCode.RightArrow) && (center.transform.rotation.eulerAngles.z + Time.deltaTime * speed) < 345)
+            {
+                center.transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed));
+            }
+
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.A) && (center.transform.rotation.eulerAngles.z+ Time.deltaTime * speed) < 165)
+            {
+                center.transform.Rotate(new Vector3(0, 0, Time.deltaTime * speed));
+            }
+            if (Input.GetKey(KeyCode.D) && (center.transform.rotation.eulerAngles.z - Time.deltaTime * speed) > 15)
+            {
+                print(center.transform.rotation.eulerAngles.z);
+                center.transform.Rotate(new Vector3(0, 0, -Time.deltaTime * speed));
+            }
+        }
+    }
+}
