45259dbbf9855f9549e15de0f5bcdbd710174d18
Adrià Biarnés (3B) <adriabiarnesbelso@enti.cat>
2018-03-12 23:51:29 +0100

    AFEGIT spinCamera.cs
    L'Script que girarà la càmara aleatoriament

---
+++ Pong+Arcanoid/Assets/Scripts/spinCamera.cs
@@ -0,0 +1,23 @@
+﻿//Aquest Script es posarà a la càmara (també podriem girar tot el mapa sense problemes, però així és una mica més senzill)
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spinCamera : MonoBehaviour {
+
+    //posar a true quan comenci la partida
+    public bool spinIsActive = false;
+    public float MAX_COOLDOWN;
+    public float SPIN_SPEED;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        if (spinIsActive)
+        {
+
+        }
+	}
+}
